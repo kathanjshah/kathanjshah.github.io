5793fbf790e51a069d0a4afa77ecbb35c38dbcd2
Kathan Jay Shah <kjshah@mun.ca>
2022-11-05 00:32:00 -0230

    commit 2

--- src/Components/body/index.js
+++ src/Components/body/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./body.css";
 import { Button, useMediaQuery } from "@mui/material";
-import peopleImage from "../../Assets/people.jpg";
+import BugReportIcon from "@mui/icons-material/BugReport";
 import menuImage from "../../Assets/menus.png";
 import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
+import { useState } from "react";
+import GTranslateIcon from "@mui/icons-material/GTranslate";
 
 function Body() {
   const matches = useMediaQuery("(min-width:720px)");
@@ -12,22 +14,34 @@ function Body() {
     maxWidth: 360,
     bgcolor: "background.paper",
   };
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
     <div className="body">
       <div className="body1">
         <div className="discription">
-          <h1>&lt;Hello World! &gt; <br/> I'm <span style={{color: '#1976d2'}}>Kathan</span> Shah</h1>
-          <p>
-            Choose your workspace or make your own.
-            {matches ? <br /> : null}Everyteam has a unique process.
-          </p>
-          <Button className="get" variant="contained" size="small">
-            Get Started
-          </Button>
+          <h1>
+            &lt;Hello World! &gt; <br /> I'm{" "}
+            <span style={{ color: "#1976d2" }}>Kathan</span> Shah
+          </h1>
+          <div className="developer">
+            <BugReportIcon />
+            <p>Full Stack Developer</p>
+          </div>
         </div>
       </div>
       <div className="body2">
-        <h3>Designed and Build by Teams for teams</h3>
+        <h3 onClick={handleClick}>
+          {!active ? (
+            <span>"આ સમય પણ વહી જશે"</span>
+          ) : (
+            <span>"This time shall pass too"</span>
+          )}
+          <GTranslateIcon className="translate" />
+        </h3>
+        <p> - Narendra Lalchand Shah(Grandfather)</p>
         <p>
           Teambo is #1 tool for managing teams and accesing tasks.
           <br />
